37cf5c64b7be6a93bede4a2048d5ed97739c6c83
alx-cpu <alx.yoi67@gmail.com>
2024-02-22 09:00:45 +0700

    feat(email): update sending email page ui and function

--- src/routes/[slug]/+page.svelte
+++ src/routes/[slug]/+page.svelte
@@ -1,13 +1,14 @@
 <script>
+	import { Spinner, Card, Button, Label, Input } from 'flowbite-svelte';
+	import { BadgeCheckSolid, ExclamationCircleSolid } from 'flowbite-svelte-icons';
+
 	let loading = false;
-	let error = null;
-	let success = null;
+	let status = null;
 
 	const handleSubmit = async (event) => {
 		let { target } = event;
 		loading = true;
-		error = null;
-		success = null;
+		status = null;
 
 		try {
 			const response = await fetch('?/sendEmail', {
@@ -18,32 +19,55 @@
 			const result = await response.json();
 
 			if (response.ok) {
-				success = 'Email berhasil dikirim';
+				status = 'success';
 			} else {
 				throw new Error(result.error || 'Something went wrong');
 			}
 		} catch (err) {
 			console.error(err);
-			error = err.message || 'Failed to send email';
+			status = 'error';
 		} finally {
 			loading = false;
 		}
 	};
 </script>
 
-<main>
+<div class="flex h-screen flex-col items-center justify-center">
 	{#if loading}
-		<p>Loading...</p>
-	{:else if error}
-		<p style="color: red;">Error: {error}</p>
-	{:else if success}
-		<p style="color: green;">{success}</p>
+		<Spinner size={12} />
+	{:else if status === 'error'}
+		<ExclamationCircleSolid class="mb-4 h-32 w-32 text-red-600" />
+		<h1
+			class="mb-3 text-2xl font-bold leading-tight text-gray-900 dark:text-white sm:text-4xl lg:text-5xl"
+		>
+			Email Gagal Terkirim
+		</h1>
+	{:else if status === 'success'}
+		<BadgeCheckSolid class="mb-4 h-32 w-32 text-primary-600" />
+		<h1
+			class="mb-3 text-2xl font-bold leading-tight text-gray-900 dark:text-white sm:text-4xl lg:text-5xl"
+		>
+			Email Berhasil Terkirim
+		</h1>
+		<p
+			class="mb-5 whitespace-normal text-left text-base font-normal text-gray-500 dark:text-gray-400 md:text-lg"
+		>
+			Email telah berhasil terkirim. Mohon segera periksa kotak masuk email Anda untuk memastikan
+			bahwa pesan tersebut telah diterima.
+		</p>
 	{:else}
-		<h1>Welcome to PKPU</h1>
-		<form on:submit={handleSubmit} >
-			<h3>Input your email</h3>
-			<input type="email" name="to" placeholder="Enter your email" />
-			<button type="submit">Submit</button>
-		</form>
+		<p class="mb-4 flex items-center justify-center text-2xl font-semibold dark:text-white lg:mb-6">
+			Welcome to PKPU
+		</p>
+		<Card size="sm" padding="lg">
+			<form class="flex flex-col space-y-6" on:submit|preventDefault={handleSubmit}>
+				<h3 class="text-xl font-medium text-gray-900 dark:text-white">Sign in to our platform</h3>
+				<Label class="space-y-2">
+					<span>Email</span>
+					<Input type="email" name="email" placeholder="name@company.com" required />
+				</Label>
+				<Button type="submit">Submit</Button>
+			</form>
+		</Card>
 	{/if}
-</main>
+</div>
