eaa34e21e5c19de810dee3205bd11a2d615f1a92
alx-cpu <alx.yoi67@gmail.com>
2024-02-22 08:59:55 +0700

    feat(email): add generate unique id, unique code generator and save to database

--- src/routes/[slug]/+page.server.js
+++ src/routes/[slug]/+page.server.js
@@ -1,61 +1,124 @@
-import { SITE_URL, EMAIL_1, EMAIL_2 } from "$env/static/private";
-import transporter from "$lib/emailSetup.server.js";
+import { SITE_URL } from '$env/static/private';
+import transporter from '$lib/emailSetup.server.js';
+import { prisma } from '$lib/prisma.server.js';
+
+async function generateUniqueRandomId(length) {
+	const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+	let result = '';
+
+	// Generate a unique ID
+	while (true) {
+		for (let i = 0; i < length; i++) {
+			const randomIndex = Math.floor(Math.random() * characters.length);
+			result += characters.charAt(randomIndex);
+		}
+
+		//   Check if the generated ID already exists in the database
+		const existingUser = await prisma.users.findUnique({
+			where: { id: result }
+		});
+
+		if (!existingUser) {
+			// If the ID is unique, break out of the loop
+			break;
+		} else {
+			// If the ID already exists, reset and try generating a new one
+			result = '';
+		}
+	}
+
+	return result;
+}
+async function generateUniqueCode(length) {
+	const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+	let result = '';
+
+	// Generate a unique code
+	while (true) {
+		for (let i = 0; i < length; i++) {
+			const randomIndex = Math.floor(Math.random() * characters.length);
+			result += characters.charAt(randomIndex);
+		}
+
+		// Check if the generated code already exists in the database
+		const existingUser = await prisma.users.findFirst({
+			where: { uniqueCode: result }
+		});
+
+		if (!existingUser) {
+			// If the code is unique, break out of the loop
+			break;
+		} else {
+			// If the code already exists, reset and try generating a new one
+			result = '';
+		}
+	}
+
+	return result;
+}
+
+const sendEmail = async (message) => {
+    try {
+        return await transporter.sendMail(message);
+      } catch (error) {
+        console.error(error);
+        throw error;
+      }
+  };
 
 export const actions = {
-    sendEmail: async ({ request }) => {
-        try {
-            const formData = await request.formData();
-            const email = formData.get("to");
+	sendEmail: async ({ request }) => {
+		try {
+			const formData = await request.formData();
+			const email = formData.get('email');
 
-            const defaultData = [
-                {
-                    email : EMAIL_1,
-                    uniquecode: 'fd82ja73h214db33',
-                    role: 'admin'
-                },
-                {
-                    email : EMAIL_2,
-                    uniquecode: 'k8d1d21hen1w43d',
-                    role: 'user'
-                }
-            ];
-            const user = defaultData.find(item => item.email === email);
-            // const subject = formData.get("subject");
-            const userId = 'H78KF';
-            const link = `${SITE_URL}/${userId}/${user.uniquecode}`;
-            let html = `<h2>Hi!</h2><p>Click the following link to access the form: <a href="${link}">${link}</a></p>`;
+            const existingUser = await prisma.users.findUnique({
+                where: { email },
+                select: {
+                    id: true,
+                    uniqueCode: true,
+                  },
+            });
 
-            const message = {
-                from: '"pkpu.co.id" <fotoarchive8@gmail.com>',
-                to: email,
-                bcc: "www.pkpu.co.id",
-                subject: 'Link to access Form Tagihan',
-                text: 'INI BODY',
-                html: html,
-                
-            };
+            let uniqueId, uniqueCode;
 
-            const sendEmail = async (message) => {
-                await new Promise((resolve, reject) => {
-                    transporter.sendMail(message, (err, info) => {
-                        if (err) {
-                            console.error(err);
-                            reject(err);
-                        } else {
-                            resolve(info);
-                        }
-                    });
+            if (!existingUser){
+                uniqueId = await generateUniqueRandomId(5);
+                uniqueCode = await generateUniqueCode(25);
+                await prisma.users.create({
+                    data: {
+                        email,
+                        id: uniqueId,
+                        uniqueCode,
+                        role: 'user'
+                    }
                 });
-            };
+            }else{
+                uniqueId = existingUser.id
+                uniqueCode = existingUser.uniqueCode
+            }
+            
+			const link = `${SITE_URL}/${uniqueId}/${uniqueCode}`;
+			let html = `<h2>Hi!</h2><p>Click the following link to access the form: <a href="${link}">${link}</a></p>`;
 
-            await sendEmail(message);
+			const message = {
+				from: '"pkpu.co.id" <fotoarchive8@gmail.com>',
+				to: email,
+				bcc: 'www.pkpu.co.id',
+				subject: 'Link to access Form Tagihan',
+				text: 'INI BODY',
+				html: html
+			};
+			await sendEmail(message);
 
-            return {
-                success: "Email is sent",
-            };
-        } catch (error) {
-            console.error(error);
-        }
-    }
+			return {
+				success: 'Email is sent'
+			};
+		} catch (error) {
+			console.error(error);
+			return {
+				error: 'Failed to send email'
+			};
+		}
+	}
 };
- 
