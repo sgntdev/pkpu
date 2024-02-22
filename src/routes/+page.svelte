383169292cc47f1ed2aca6af4bb3bfd179c81a13
alx-cpu <alx.yoi67@gmail.com>
2024-02-22 08:57:57 +0700

    feat(home): update home page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,18 +1,41 @@
-<h1>Home</h1>
-<h3>List Debitur PKPU</h3>
-<table border="1px solid">
-    <tr>
-        <th>No</th>
-        <th>Nama</th>
-        <th>Tanggal Sidang</th>
-        <th>Tempat Sidang</th>
-        <th>Link</th>
-    </tr>
-    <tr>
-        <td>1</td>
-        <td>PT ABC</td>
-        <td>4 Januari 2024</td>
-        <td>Jakarta</td>
-        <td><a href="/pt-abc">PT ABC</a></td>
-    </tr>
-</table>
+<script>
+	import {
+		Table,
+		TableBody,
+		TableBodyCell,
+		TableBodyRow,
+		TableHead,
+		TableHeadCell,
+		Checkbox,
+		TableSearch
+	} from 'flowbite-svelte';
+</script>
+
+<div class="h-">
+	<p class="mb-4 text-3xl font-semibold">List Debitur PKPU</p>
+	<div class="overflow-hidden border border-gray-200 dark:border-gray-700 md:rounded-lg">
+		<Table>
+			<TableHead>
+				<TableHeadCell>No</TableHeadCell>
+				<TableHeadCell>Nama</TableHeadCell>
+				<TableHeadCell>Tanggal Sidang</TableHeadCell>
+				<TableHeadCell>Tempat Sidang</TableHeadCell>
+				<TableHeadCell>Link</TableHeadCell>
+			</TableHead>
+			<TableBody class="divide-y">
+				<TableBodyRow>
+					<TableBodyCell>1</TableBodyCell>
+					<TableBodyCell>PT ABC</TableBodyCell>
+					<TableBodyCell>4 Januari 2024</TableBodyCell>
+					<TableBodyCell>Jakarta</TableBodyCell>
+					<TableBodyCell>
+						<a
+							href="/pt-abc"
+							class="font-medium text-primary-600 hover:underline dark:text-primary-500">PT ABC</a
+						>
+					</TableBodyCell>
+				</TableBodyRow>
+			</TableBody>
+		</Table>
+	</div>
+</div>
