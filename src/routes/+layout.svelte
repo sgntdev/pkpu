cd9e2cfaf864444744a0743d1d505632f8c0a794
alx-cpu <alx.yoi67@gmail.com>
2024-02-25 10:05:50 +0700

    style(layout): update layout style

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -13,14 +13,14 @@
 	} from 'flowbite-svelte';
 </script>
 
-<Navbar let:hidden let:toggle class="relative top-0 left-0 right-0 z-50">
+<Navbar let:hidden let:toggle class="relative top-0 left-0 right-0 z-50 border bg-white border-gray-200 md:py-4">
 	<NavBrand href="/">
 		<span class="self-center whitespace-nowrap text-xl font-semibold dark:text-white"> PKPU </span>
 	</NavBrand>
 </Navbar>
-<div class="relative mx-auto w-full px-4 md:px-20">
+<div class="relative mx-auto min-h-full max-w-7xl py-6 px-4">
 	<slot />
 </div>
-<!-- <Footer footerType="logo" class="relative bottom-0 left-0 right-0">
+<Footer footerType="logo" class="relative bottom-0 left-0 right-0">
 	<FooterCopyright href="/" by="pkpu.co.id" />
-  </Footer> -->
+  </Footer>
