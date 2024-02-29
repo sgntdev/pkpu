3a15b142345cece315c02a2385179fb9b2ee26cc
alx-cpu <alx.yoi67@gmail.com>
2024-02-29 17:25:03 +0700

    feat(email): update from form action to api and add validation

---
+++ src/routes/(app)/[slug]/+layout@.svelte
@@ -0,0 +1,6 @@
+<svelte:head>
+	<title>PKPU</title>
+</svelte:head>
+<main class="min-h-screen flex flex-col justify-center items-center">
+    <slot/>
+</main>
--- src/routes/(app)/[slug]/+page.svelte
+++ src/routes/(app)/[slug]/+page.svelte
@@ -1,73 +1,86 @@
 <script>
-	import { Spinner, Card, Button, Label, Input } from 'flowbite-svelte';
+	import { Spinner, Card, Button, Label, Input, Helper } from 'flowbite-svelte';
 	import { BadgeCheckSolid, ExclamationCircleSolid } from 'flowbite-svelte-icons';
 
 	let loading = false;
 	let status = null;
-
+	let form;
 	const handleSubmit = async (event) => {
-		let { target } = event;
-		loading = true;
+		const data = new FormData(event.currentTarget);
+
 		status = null;
+		loading = true;
 
 		try {
-			const response = await fetch('?/sendEmail', {
+			const response = await fetch('/api/users', {
 				method: 'POST',
-				body: new FormData(target)
+				body: data
 			});
 
 			const result = await response.json();
-
-			if (response.ok) {
+			form = result;
+			if (result.success) {
 				status = 'success';
 			} else {
-				throw new Error(result.error || 'Something went wrong');
+				if (result.errors.length === 0) {
+					status = 'error';
+				}
 			}
 		} catch (err) {
 			console.error(err);
-			status = 'error';
 		} finally {
 			loading = false;
 		}
 	};
 </script>
 
-<main class="flex flex-col items-center justify-center md:h-screen">
-	{#if loading}
-		<Spinner size={12} />
-	{:else if status === 'error'}
-		<ExclamationCircleSolid class="mb-4 h-32 w-32 text-red-600" />
-		<h1
-			class="mb-3 text-2xl font-bold leading-tight text-gray-900 dark:text-white sm:text-4xl lg:text-5xl"
-		>
-			Email Gagal Terkirim
-		</h1>
-	{:else if status === 'success'}
-		<BadgeCheckSolid class="mb-4 h-32 w-32 text-primary-600" />
-		<h1
-			class="mb-3 text-2xl font-bold leading-tight text-gray-900 dark:text-white sm:text-4xl lg:text-5xl"
-		>
-			Email Berhasil Terkirim
-		</h1>
-		<p
-			class="mb-5 whitespace-normal text-left text-base font-normal text-gray-500 dark:text-gray-400 md:text-lg"
-		>
-			Email telah berhasil terkirim. Mohon segera periksa kotak masuk email Anda untuk memastikan
-			bahwa pesan tersebut telah diterima.
-		</p>
-	{:else}
-		<p class="mb-4 flex items-center justify-center text-2xl font-semibold dark:text-white lg:mb-6">
-			Welcome to PKPU
-		</p>
-		<Card size="sm" padding="lg">
-			<form class="flex flex-col space-y-6" on:submit|preventDefault={handleSubmit}>
-				<h3 class="text-xl font-medium text-gray-900 dark:text-white">Sign in to our platform</h3>
-				<Label class="space-y-2">
-					<span>Email</span>
-					<Input type="email" name="email" placeholder="name@company.com" required />
-				</Label>
-				<Button type="submit">Submit</Button>
-			</form>
-		</Card>
-	{/if}
-</main>
+{#if loading}
+	<Spinner size={12} />
+{:else if status === 'error'}
+	<ExclamationCircleSolid class="mb-4 h-32 w-32 text-red-600" />
+	<h1
+		class="mb-3 text-2xl font-bold leading-tight text-gray-900 dark:text-white sm:text-4xl lg:text-5xl"
+	>
+		Email Gagal Terkirim
+	</h1>
+{:else if status === 'success'}
+	<BadgeCheckSolid class="mb-4 h-32 w-32 text-primary-600" />
+	<h1
+		class="mb-3 text-2xl font-bold leading-tight text-gray-900 dark:text-white sm:text-4xl lg:text-5xl"
+	>
+		Email Berhasil Terkirim
+	</h1>
+	<p
+		class="mb-5 whitespace-normal text-left text-base font-normal text-gray-500 dark:text-gray-400 md:text-lg"
+	>
+		Email telah berhasil terkirim. Mohon segera periksa kotak masuk email Anda untuk memastikan
+		bahwa pesan tersebut telah diterima.
+	</p>
+{:else}
+	<p class="mb-4 flex items-center justify-center text-2xl font-semibold dark:text-white lg:mb-6">
+		Welcome to PKPU
+	</p>
+	<Card size="sm" padding="lg">
+		<form class="flex flex-col space-y-6" on:submit|preventDefault={handleSubmit}>
+			<h3 class="text-xl font-medium text-gray-900 dark:text-white">Sign in to our platform</h3>
+			<Label
+				class="space-y-2"
+				color={form?.errors?.find((error) => error.field === 'email') ? 'red' : undefined}
+			>
+				<span>Email</span>
+				<Input
+					type="text"
+					name="email"
+					placeholder="name@company.com"
+					color={form?.errors?.find((error) => error.field === 'email') ? 'red' : undefined}
+				/>
+				{#if form?.errors?.find((error) => error.field === 'email')}
+					<Helper class="mt-2" color="red"
+						>{form?.errors?.find((error) => error.field === 'email').message}</Helper
+					>
+				{/if}
+			</Label>
+			<Button type="submit">Submit</Button>
+		</form>
+	</Card>
+{/if}
