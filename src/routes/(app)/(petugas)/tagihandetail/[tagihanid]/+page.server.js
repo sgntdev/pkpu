0e83feab3f38e15f5b89b06e52021e4d9c8073a3
Syifa'udin Ridho <starcreator.work@gmail.com@email.com>
2024-05-27 18:17:23 +0700

    fix bug tagihan, dokumen tagihan

---
+++ src/routes/(app)/(petugas)/tagihandetail/[tagihanid]/+page.server.js
@@ -0,0 +1,32 @@
+import { error, redirect } from '@sveltejs/kit';
+
+export async function load({ params, fetch, locals }) {
+	const { user, token } = locals;
+	const { tagihanid } = params;
+	const tagihanId = parseInt(tagihanid);
+	if (!user) {
+		redirect(303, '/');
+	} else {
+		if (user.roleId === 1 || user.roleId === 2) {
+			const tagihanResponse = await fetch(`/api/tagihan/${tagihanId}?userId=${user.id}`, {
+				method: 'GET',
+				headers: {
+					'Content-Type': 'application/json',
+					Authorization: `Bearer ${token}`
+				}
+			});
+			const tagihan = await tagihanResponse.json();
+			return {
+				status: 200,
+				body: {
+					roleId: user.roleId,
+					userId: user.id,
+					tagihan: tagihan.data,
+					token
+				}
+			};
+		} else {
+			error(404, 'Page Not Found');
+		}
+	}
+}
