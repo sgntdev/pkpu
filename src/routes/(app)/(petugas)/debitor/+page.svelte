f35e2195ebfede28e422d990a0ebcb35498af7f4
Syifa'udin Ridho <starcreator.work@gmail.com@email.com>
2024-05-28 14:33:44 +0700

    Resolved Button

--- src/routes/(app)/(petugas)/debitor/+page.svelte
+++ src/routes/(app)/(petugas)/debitor/+page.svelte
@@ -10,8 +10,7 @@
 		BreadcrumbItem,
 		Button,
 		Toast,
-		Modal,
-		ButtonGroup
+		Modal
 	} from 'flowbite-svelte';
 	import { CheckCircleSolid, ExclamationCircleOutline, XCircleSolid } from 'flowbite-svelte-icons';
 	import { page } from '$app/stores';
@@ -160,46 +159,44 @@
 							<TableBodyCell>{data.tempatSidang}</TableBodyCell>
 							{#if roleId === 1}
 								<TableBodyCell>
-									<ButtonGroup>
-										<Button outline color="blue" data-sveltekit-reload href={`debitor/${data.id}`}>
-											<svg
-												xmlns="http://www.w3.org/2000/svg"
+									<Button data-sveltekit-reload href={`debitor/${data.id}`}>
+										<svg
+											xmlns="http://www.w3.org/2000/svg"
+											fill="currentColor"
+											class="h-5 w-5 shrink-0"
+											role="img"
+											aria-label="edit solid"
+											viewBox="0 0 24 24"
+											><path
 												fill="currentColor"
-												class="h-5 w-5 shrink-0"
-												role="img"
-												aria-label="edit solid"
-												viewBox="0 0 24 24"
-												><path
-													fill="currentColor"
-													fill-rule="evenodd"
-													d="M11.3 6.2H5a2 2 0 0 0-2 2V19a2 2 0 0 0 2 2h11c1.1 0 2-1 2-2.1V11l-4 4.2c-.3.3-.7.6-1.2.7l-2.7.6c-1.7.3-3.3-1.3-3-3.1l.6-2.9c.1-.5.4-1 .7-1.3l3-3.1Z"
-													clip-rule="evenodd"
-												></path><path
-													fill="currentColor"
-													fill-rule="evenodd"
-													d="M19.8 4.3a2.1 2.1 0 0 0-1-1.1 2 2 0 0 0-2.2.4l-.6.6 2.9 3 .5-.6a2.1 2.1 0 0 0 .6-1.5c0-.2 0-.5-.2-.8Zm-2.4 4.4-2.8-3-4.8 5-.1.3-.7 3c0 .3.3.7.6.6l2.7-.6.3-.1 4.7-5Z"
-													clip-rule="evenodd"
-												></path></svg
-											>
-										</Button>
-										<Button outline color="red" on:click={() => openDeleteModal(data.id)}>
-											<svg
-												xmlns="http://www.w3.org/2000/svg"
+												fill-rule="evenodd"
+												d="M11.3 6.2H5a2 2 0 0 0-2 2V19a2 2 0 0 0 2 2h11c1.1 0 2-1 2-2.1V11l-4 4.2c-.3.3-.7.6-1.2.7l-2.7.6c-1.7.3-3.3-1.3-3-3.1l.6-2.9c.1-.5.4-1 .7-1.3l3-3.1Z"
+												clip-rule="evenodd"
+											></path><path
 												fill="currentColor"
-												class="h-5 w-5 shrink-0"
-												role="img"
-												aria-label="trash bin solid"
-												viewBox="0 0 24 24"
-												><path
-													fill="currentColor"
-													fill-rule="evenodd"
-													d="M8.6 2.6A2 2 0 0 1 10 2h4a2 2 0 0 1 2 2v2h3a1 1 0 1 1 0 2v12a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V8a1 1 0 0 1 0-2h3V4c0-.5.2-1 .6-1.4ZM10 6h4V4h-4v2Zm1 4a1 1 0 1 0-2 0v8a1 1 0 1 0 2 0v-8Zm4 0a1 1 0 1 0-2 0v8a1 1 0 1 0 2 0v-8Z"
-													clip-rule="evenodd"
-												></path></svg
-											>
-										</Button>
-									</ButtonGroup>
-								</TableBodyCell>
+												fill-rule="evenodd"
+												d="M19.8 4.3a2.1 2.1 0 0 0-1-1.1 2 2 0 0 0-2.2.4l-.6.6 2.9 3 .5-.6a2.1 2.1 0 0 0 .6-1.5c0-.2 0-.5-.2-.8Zm-2.4 4.4-2.8-3-4.8 5-.1.3-.7 3c0 .3.3.7.6.6l2.7-.6.3-.1 4.7-5Z"
+												clip-rule="evenodd"
+											></path></svg
+										>
+									</Button>
+									<Button color="red" on:click={() => openDeleteModal(data.id)}>
+										<svg
+											xmlns="http://www.w3.org/2000/svg"
+											fill="currentColor"
+											class="h-5 w-5 shrink-0"
+											role="img"
+											aria-label="trash bin solid"
+											viewBox="0 0 24 24"
+											><path
+												fill="currentColor"
+												fill-rule="evenodd"
+												d="M8.6 2.6A2 2 0 0 1 10 2h4a2 2 0 0 1 2 2v2h3a1 1 0 1 1 0 2v12a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V8a1 1 0 0 1 0-2h3V4c0-.5.2-1 .6-1.4ZM10 6h4V4h-4v2Zm1 4a1 1 0 1 0-2 0v8a1 1 0 1 0 2 0v-8Zm4 0a1 1 0 1 0-2 0v8a1 1 0 1 0 2 0v-8Z"
+												clip-rule="evenodd"
+											></path></svg
+										></Button
+									></TableBodyCell
+								>
 							{/if}
 						</TableBodyRow>
 					{/each}
