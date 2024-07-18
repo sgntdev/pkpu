ad6cc32b5b8af18e8699bbc2d52b982e116a4212
alx-cpu <alx.yoi67@gmail.com>
2024-07-18 18:29:58 +0700

    feat(datepicker): update datepicker validation

--- src/lib/components/DatePicker.svelte
+++ src/lib/components/DatePicker.svelte
@@ -5,6 +5,7 @@
 	export let startDate = null;
 	export let dateFormat = 'dd MMMM yyyy';
 	export let isOpen = false;
+	export let invalid = false;
 
 	const toggleDatePicker = () => (isOpen = !isOpen);
 
@@ -20,7 +21,7 @@
 <DatePicker bind:isOpen bind:startDate>
 	<input
 		type="text"
-		class="datepicker"
+		class="datepicker {invalid ? 'invalid' : ''}"
 		placeholder="Pilih tanggal"
 		bind:value={formattedStartDate}
 		on:click={toggleDatePicker}
@@ -29,16 +30,23 @@
 
 <style>
 	.datepicker {
-		border: 1px solid rgb(209 213 219 / var(--tw-border-opacity));
+		border: 1px solid rgb(209 213 219);
 		border-radius: 0.5rem;
 		padding: 0.625rem;
 		width: 100%;
-		color: rgb(17 24 39 / var(--tw-text-opacity));
-		background-color: rgb(249 250 251 / var(--tw-bg-opacity));
+		color: rgb(17 24 39);
+		background-color: rgb(249 250 251);
 		line-height: 1.25rem;
 		font-size: 0.875rem;
 	}
 	.datepicker:focus {
-		border-color: rgb(63 131 248 / var(--tw-border-opacity));
+		border-color: rgb(63 131 248);
+	}
+	.datepicker.invalid{
+		border-color: rgb(240 82 82);
+		background-color: rgb(253 242 242);
+	}
+	.datepicker.invalid::placeholder {
+		color: rgb(119 29 29);
 	}
 </style>
