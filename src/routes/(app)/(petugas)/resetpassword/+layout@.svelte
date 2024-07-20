d6bbff65cc95f040091fb6ddcc7f8d074ab412de
alx-cpu <alx.yoi67@gmail.com>
2024-07-20 11:56:23 +0700

    style(layout): update layout

--- src/routes/(app)/(petugas)/resetpassword/+layout@.svelte
+++ src/routes/(app)/(petugas)/resetpassword/+layout@.svelte
@@ -1,12 +1,6 @@
 
 <script>
-	import { injectSpeedInsights } from '@vercel/speed-insights/sveltekit';
-	injectSpeedInsights();
-	import { dev } from '$app/environment';
-	import { inject } from '@vercel/analytics';
-	inject({ mode: dev ? 'development' : 'production' });
 	import '../../../../app.pcss';
-	
 </script>
 
 <svelte:head>
