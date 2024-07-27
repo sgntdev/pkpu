8896d9ec1f4bc295049737c98a1c470b043c4b30
alx-cpu <alx.yoi67@gmail.com>
2024-07-27 19:35:44 +0700

    test fix authentication set cookie

--- src/routes/(app)/logout/+page.server.js
+++ src/routes/(app)/logout/+page.server.js
@@ -1,8 +1,15 @@
 import { redirect } from '@sveltejs/kit';
 
 /** @type {import('./$types').PageServerLoad} */
-export async function load({cookies}) {
-    await cookies.delete('AuthorizationToken', { path: '/' });
-    console.log('clear cookie');
-    redirect(302, '/')
-};
+export async function load({ cookies }) {
+	const session = cookies.get('AuthorizationToken');
+	if (session !== '') {
+		cookies.set('AuthorizationToken', '', {
+			path: '/'
+		});
+		console.log('clear cookie');
+	} else {
+		await cookies.delete('AuthorizationToken', { path: '/' });
+	}
+	redirect(302, '/');
+}
