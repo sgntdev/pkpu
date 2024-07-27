b3f6566104a370e8fa2a3fc17b9e8748064c0a9d
alx-cpu <alx.yoi67@gmail.com>
2024-07-28 05:46:41 +0700

    fix authentication set cookie

--- src/routes/(app)/logout/+page.server.js
+++ src/routes/(app)/logout/+page.server.js
@@ -1,21 +1,5 @@
 import { redirect } from '@sveltejs/kit';
-
-/** @type {import('./$types').PageServerLoad} */
 export async function load({ cookies }) {
-	const session = cookies.get('AuthorizationToken');
-	// if (session !== '') {
-	// 	cookies.set('AuthorizationToken', '', {
-	// 		path: '/',
-	// 		httpOnly: true,
-	// 		secure: true,
-	// 		sameSite: 'strict',
-	// 		maxAge: 60 * 60 * 24
-	// 	});
-	// 	console.log('set empty cookie');
-	// } else {
-        console.log('cookie empty and logout' , session);
-		await cookies.delete('AuthorizationToken', { path: '/' });
-		await cookies.delete('authToken', { path: '/' });
-	// }
+	await cookies.delete('AuthorizationToken', { path: '/' });
 	redirect(302, '/');
 }
--- src/routes/(temp)/verify/[[debitorUid]]/[uniqueCode]/+page.server.js
+++ src/routes/(temp)/verify/[[debitorUid]]/[uniqueCode]/+page.server.js
@@ -3,24 +3,13 @@ import { prisma } from '$lib/prisma.server.js';
 
 export async function load({ params, cookies, fetch }) {
 	const { uniqueCode } = params;
-	let userverif;
-	let result;
-
-	try {
-		userverif = await prisma.UserVerify.findUnique({
-			where: { uniqueCode },
-			select: {
-				email: true
-			}
-		});
-		
-		if (!userverif) {
-			throw error(404, {
-				message: 'User Not Found',
-				description: 'Please ensure that the information you entered is correct'
-			});
+	const userverif = await prisma.UserVerify.findUnique({
+		where: { uniqueCode },
+		select: {
+			email: true
 		}
-
+	});
+	if (userverif) {
 		const res = await fetch(`/api/auth`, {
 			method: 'POST',
 			headers: {
@@ -28,52 +17,43 @@ export async function load({ params, cookies, fetch }) {
 			},
 			body: JSON.stringify(params)
 		});
-		
 		if (!res.ok) {
 			if (res.status === 400) {
-				throw error(res.status, {
+				error(res.status, {
 					message: 'Expired Unique Code',
 					description: 'Please use a valid verification link'
 				});
 			} else {
-				throw error(res.status, 'Not Found');
+				error(res.status, 'Not Found');
 			}
 		}
-
-		result = await res.json();
-
+		const result = await res.json();
 		const user = await prisma.user.findUnique({
 			where: { email: userverif.email },
 			select: {
 				roleId: true
 			}
 		});
-
-		if (!result.authToken) {
-			throw error(404, {
+		if (result.authToken) {
+			await cookies.delete('AuthorizationToken', { path: '/' });
+			cookies.set('AuthorizationToken', result.authToken, {
+				path: '/',
+				httpOnly: true,
+				secure: true,
+				sameSite: 'strict',
+				maxAge: 60 * 60 * 24 // one day
+			});
+			return { success: true, user: userverif.email, roleId: user?.roleId };
+		} else {
+			error(404, {
 				message: 'Token Not Found',
 				description: 'Please ensure that the information you entered is correct'
 			});
 		}
-
-		cookies.set('AuthorizationToken', result.authToken, {
-			path: '/',
-			httpOnly: true,
-			secure: true,
-			sameSite: 'strict',
-			maxAge: 60 * 60 * 24 // one day
+	} else {
+		error(404, {
+			message: 'User Not Found',
+			description: 'Please ensure that the information you entered is correct'
 		});
-		cookies.set('authToken', result.authToken, {
-			path: '/',
-			httpOnly: true,
-			secure: true,
-			maxAge: 60 * 60 * 24 // one day
-		});
-		let getCookie = cookies.get('AuthorizationToken')
-		console.log('set cookie', getCookie);
-		return { success: true, user: userverif.email, roleId: user?.roleId };
-	} catch (err) {
-		console.error('Error in load function:', err);
-		throw err;
 	}
 }
